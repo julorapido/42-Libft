7fbb169e157e43f3b55b05903c3418d392f45ca9
Jules Sainthorant <jsaintho@made-f0Ar2s8.clusters.42paris.fr>
2024-06-04 16:43:24 +0200

    calloc fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jsaintho <jsaintho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 18:26:10 by julessainth       #+#    #+#             */
-/*   Updated: 2024/05/29 15:54:47 by jsaintho         ###   ########.fr       */
+/*   Updated: 2024/06/04 16:41:26 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,15 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*c_alloc;
-	size_t	i;
 
-	if (size > 2147483647 || count > 2147483647
-		|| ((count == 0 || size == 0) && !(size == 0 && count == 0)))
+	if (size >= 2147483647 || count >= 2147483647 
+		|| count * size >= SIZE_MAX)
 		return (NULL);
-	if ((int)size <= 0 || (int)count <= 0)
-	{
-		c_alloc = (void *)(malloc (1));
-		*(unsigned char *)(c_alloc) = 0;
-		return (c_alloc);
-	}
+	if ((int) size * (int) count < 0)
+		return (NULL);
 	c_alloc = (void *)(malloc(count * size));
 	if (!c_alloc)
 		return (NULL);
-	i = 0;
-	while (i < (size * count))
-	{
-		*(unsigned char *)(c_alloc + i) = 0;
-		i++;
-	}
+	ft_bzero(c_alloc, count * size);
 	return (c_alloc);
 }
