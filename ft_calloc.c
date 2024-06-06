b7db3852744c454285856aa6f1c105f6f4ba1aaf
Jules Sainthorant <jsaintho@made-f0Ar3s9.clusters.42paris.fr>
2024-06-06 11:49:12 +0200

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jsaintho <jsaintho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 18:26:10 by julessainth       #+#    #+#             */
-/*   Updated: 2024/06/04 16:41:26 by jsaintho         ###   ########.fr       */
+/*   Updated: 2024/06/06 11:46:51 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*c_alloc;
 
-	if (size >= 2147483647 || count >= 2147483647 
-		|| count * size >= SIZE_MAX)
-		return (NULL);
-	if ((int) size * (int) count < 0)
+	if (size && SIZE_MAX / size < count)
 		return (NULL);
 	c_alloc = (void *)(malloc(count * size));
 	if (!c_alloc)
