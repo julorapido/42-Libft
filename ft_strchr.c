a68f244cb1cecca16df5eedc1cc5cdc9e9699e30
Jules Sainthorant <jsaintho@made-f0Dr5s6.clusters.42paris.fr>
2024-07-15 16:23:34 +0200

    lib fix

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,27 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsaintho <jsaintho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jsaintho <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/04 14:52:48 by julessainth       #+#    #+#             */
-/*   Updated: 2024/05/29 15:56:49 by jsaintho         ###   ########.fr       */
+/*   Created: 2024/07/15 15:45:35 by jsaintho          #+#    #+#             */
+/*   Updated: 2024/07/15 16:22:45 by jsaintho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *string, int searchedChar )
 {
-	size_t	i;
+	char	*str;
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		if ((unsigned char)s[i] == (unsigned char)c)
-			return ((char *) &(s[i]));
-		i++;
-	}
-	if ((unsigned char)s[i] == '\0' && (unsigned char)c == '\0')
-		return ((char *) &(s[i]));
-	return (NULL);
+	str = (char *)string;
+	while (*str != searchedChar && *str != 0)
+		str++;
+	if (*str == searchedChar)
+		return (str);
+	else
+		return (NULL);
 }
